d33382ca040c1633cd6ab3be62c79ecf15e6c20b
n-uwayo <n.uwayo@alustudent.com>
2023-09-12 07:53:52 -0700

    minimum

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+
 """Minimum Operations"""
+
 def minOperations(n):
     if n <= 1:
         return 0
@@ -7,12 +9,11 @@ def minOperations(n):
     dp = [0] * (n + 1)
 
     for i in range(2, n + 1):
-        dp[i] = i 
- # Initialize with the maximum possible operations
+        dp[i] = i  # Initialize with the maximum possible operations
 
         for j in range(2, i):
             if i % j == 0:
                 dp[i] = min(dp[i], dp[j] + (i // j))
 
-    return dp[n] if dp[n] != float('inf') else 0
+    return dp[n]
 
