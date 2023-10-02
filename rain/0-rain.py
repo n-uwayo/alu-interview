b30f0860cb4f04b053f03d4882549d06b262ee0e
n-uwayo <n.uwayo@alustudent.com>
2023-10-02 02:48:07 -0700

    rain

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,20 +1,30 @@
+Certainly! Here's a documentation for the provided Python code:
+
+```python
 #!/usr/bin/python3
 """
 Given a list of non-negative integers representing walls of width 1,
 calculate how much water will be retained after it rains.
 """
 
-
 def rain(walls):
-  """
-    Function that gets the water drop from walls.
+    """
+    Calculate the amount of water that can be retained after rainfall.
+
+    This function takes a list of non-negative integers 'walls', where each
+    integer represents the height of a wall at a specific position. It calculates
+    and returns the amount of water that can be trapped between the walls after
+    rainfall.
 
     Args:
-        param1: walls wallsay structure.
+        walls (list of int): A list of non-negative integers representing wall heights.
 
     Returns:
-        The water inside walls
+        int: The total amount of water that can be retained between the walls.
 
+    Example:
+        >>> rain([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])
+        6
     """
     if not walls:
         return 0
