d3bb55d11e58f6aa50b19b6d54eeaf2a901898a0
n-uwayo <n.uwayo@alustudent.com>
2023-10-02 03:09:05 -0700

    rain

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -4,15 +4,18 @@ Given a list of non-negative integers representing walls of width 1,
 calculate how much water will be retained after it rains.
 """
 
+
 def rain(walls):
     """
     Calculate the amount of water that can be retained after rainfall.
 
     Args:
-        walls (list of int): A list of non-negative integers representing wall heights.
+        walls (list of int): A list of non-negative integers 
+        representing wall heights.
 
     Returns:
-        int: The total amount of water that can be retained between the walls. Returns 0 for empty input.
+        int: The total amount of water that can be retained between the walls.
+             Returns 0 for empty input.
 
     Example:
         >>> rain([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])
